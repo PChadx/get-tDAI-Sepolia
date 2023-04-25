bd7d33d1fb297465554315c4492dbd53d68fbc43
PChadx <ChadTrivia@protonmail.com>
2023-04-25 17:40:37 +0100

    removed 2 web3. req

--- gettDAI.js
+++ gettDAI.js
@@ -6,12 +6,17 @@ const web3 = new Web3(`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY
 
 const tokenAddress = '0x53844f9577c2334e541aec7df7174ece5df1fcf0'; // DAI token address on Seoplia testnet
 const tokenContract = new web3.eth.Contract(contractABI, tokenAddress);
-const DELAY = 12000;
+const DELAY = 5000;
 //wait 12 seconds
 const MAX_GAS_FEE = '5'; 
 // Set your desired maximum fee per gas in gwei
 const PRIORITY_FEE = '2.3'; 
 // Set your desired maximum priority fee per gas in gwei
+
+const GWEI_DECIMALS = 10 ** 9;
+
+const MAX_GAS_FEE_WEI = (MAX_GAS_FEE * GWEI_DECIMALS).toString();
+const PRIORITY_FEE_WEI = (PRIORITY_FEE * GWEI_DECIMALS).toString();
 
 let numTries = 0;
 console.log('========================================================');
@@ -32,8 +37,8 @@ async function createToken() {
   const result = await tokenContract.methods.create(process.env.YOUR_ADDRESS).send({
     from: account.address,
     gas: 200000, // You can adjust the gas limit to suit your needs
-    maxFeePerGas: web3.utils.toWei(MAX_GAS_FEE, 'gwei'), 
-    maxPriorityFeePerGas: web3.utils.toWei(PRIORITY_FEE, 'gwei') 
+    maxFeePerGas: MAX_GAS_FEE_WEI,
+    maxPriorityFeePerGas: PRIORITY_FEE_WEI
   });
 
   numTries++; // Increment the number of tries after each call
